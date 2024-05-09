3c9a75ad79cfb5cfadebffd1fc9ed47afe354fee
Robert Dixon <robmsc24@gmail.com>
2024-05-09 02:25:33 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -31,7 +31,7 @@ const restricted = (req, res, next) => {
   })
 };
 
-const only = (role_name) => (req, res, next) => {
+const only = role_name => (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -42,7 +42,11 @@ const only = (role_name) => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
-  next();
+  if (role_name === req.decodedToken.role_name) {
+    next()
+  } else {
+    next({ status: 403, message: 'This is not for you' })
+  }
 };
 
 
