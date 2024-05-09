923ab6096822e1e91ef85fb97baf61c8c129b082
Robert Dixon <robmsc24@gmail.com>
2024-05-08 22:13:23 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,4 +1,4 @@
-const db = require('../../data/db-config.js');
+const db = require("../../data/db-config.js");
 
 function find() {
   /**
@@ -18,9 +18,9 @@ function find() {
       }
     ]
    */
-    return db('users')
-    .join('roles', 'users.role_id', 'users.role_id')
-    .select('user_id', 'username', 'role_name')
+  return db("users")
+    .join("roles", "users.role_id", "users.role_id")
+    .select("user_id", "username", "role_name");
 }
 
 function findBy(filter) {
@@ -37,6 +37,10 @@ function findBy(filter) {
       }
     ]
    */
+  return db("users")
+    .join("roles", "users.role_id", "users.role_id")
+    .select("user_id", "username", "password", "role_name")
+    .where(filter);
 }
 
 function findById(user_id) {
@@ -70,21 +74,26 @@ function findById(user_id) {
     "role_name": "team lead"
   }
  */
-async function add({ username, password, role_name }) { // done for you
-  let created_user_id
-  await db.transaction(async trx => {
-    let role_id_to_use
-    const [role] = await trx('roles').where('role_name', role_name)
+async function add({ username, password, role_name }) {
+  // done for you
+  let created_user_id;
+  await db.transaction(async (trx) => {
+    let role_id_to_use;
+    const [role] = await trx("roles").where("role_name", role_name);
     if (role) {
-      role_id_to_use = role.role_id
+      role_id_to_use = role.role_id;
     } else {
-      const [role_id] = await trx('roles').insert({ role_name: role_name })
-      role_id_to_use = role_id
+      const [role_id] = await trx("roles").insert({ role_name: role_name });
+      role_id_to_use = role_id;
     }
-    const [user_id] = await trx('users').insert({ username, password, role_id: role_id_to_use })
-    created_user_id = user_id
-  })
-  return findById(created_user_id)
+    const [user_id] = await trx("users").insert({
+      username,
+      password,
+      role_id: role_id_to_use,
+    });
+    created_user_id = user_id;
+  });
+  return findById(created_user_id);
 }
 
 module.exports = {
